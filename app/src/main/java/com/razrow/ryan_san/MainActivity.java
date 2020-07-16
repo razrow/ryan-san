cf0b5ee5e0a1d61e23b6c2bdbca0576c39ed11fd
Rowan Briggs <razrow@gmail.com>
2020-07-15 21:36:21 -0700

    Added preload screen.

--- app/src/main/java/com/razrow/ryan_san/MainActivity.java
+++ app/src/main/java/com/razrow/ryan_san/MainActivity.java
@@ -2,21 +2,30 @@ package com.razrow.ryan_san;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.os.Handler;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     ImageView mImageView;
-    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
         mImageView=findViewById(R.id.RyanPic);
-        mButton=findViewById(R.id.StartButton);
     }
 }
