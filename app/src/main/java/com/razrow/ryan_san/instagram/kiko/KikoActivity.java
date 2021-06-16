4dd0a50ed7857a44385fff85a0e29f8b87b83eec
razrow <razrow@gmail.com>
2021-06-15 22:29:27 -0700

    worked on routes for ig

---
+++ app/src/main/java/com/razrow/ryan_san/instagram/kiko/KikoActivity.java
@@ -0,0 +1,16 @@
+package com.razrow.ryan_san.instagram.kiko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.razrow.ryan_san.R;
+
+public class KikoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiko);
+    }
+}
---
+++ app/src/main/java/com/razrow/ryan_san/instagram/yolanda/YolandaActivity.java
@@ -0,0 +1,16 @@
+package com.razrow.ryan_san.instagram.yolanda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.razrow.ryan_san.R;
+
+public class YolandaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yolanda);
+    }
+}
