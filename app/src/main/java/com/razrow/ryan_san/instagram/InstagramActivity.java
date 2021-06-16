4f501e47ef7e9afab80504af1452248c3cc54af6
razrow <razrow@gmail.com>
2021-06-15 21:58:28 -0700

    added drop ddown option on ig route

--- app/src/main/java/com/razrow/ryan_san/instagram/InstagramActivity.java
+++ app/src/main/java/com/razrow/ryan_san/instagram/InstagramActivity.java
@@ -3,14 +3,44 @@ package com.razrow.ryan_san.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.razrow.ryan_san.R;
 
-public class InstagramActivity extends AppCompatActivity {
+public class InstagramActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinnerInstagram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instagram);
+
+        spinnerInstagram = findViewById(R.id.instagramDmsDropdown);
+        spinnerInstagram.setOnItemSelectedListener(this);
+
+        String[] instagramDms = getResources().getStringArray(R.array.instagramdms);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, instagramDms);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerInstagram.setAdapter(adapter);
+
+        
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(parent.getId() == R.id.instagramDmsDropdown){
+            String valueFromSpinner = parent.getItemAtPosition(position).toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
